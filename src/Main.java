921876e07127ac4e57316722b0280d0c4285dabe
dglee <ldg6153@gmail.com>
2024-05-05 17:09:14 +0900

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * PackageName :
+ * FileName : ${NAME}
+ * Author : dglee
+ * Create : 5/5/24 5:06 PM
+ * Description :
+ **/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
